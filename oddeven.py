d45f81515ce6f22097505131f3086ea5adbc964b
SHILPA GRACE V <67222800+SHILPA-GRACE-V@users.noreply.github.com>
2020-06-24 19:50:57 +0530

    Add files via upload

---
+++ oddeven.py
@@ -0,0 +1,10 @@
+even=[]
+odd=[]
+for i in range(26):
+    if i%2 == 0 :
+        even.append(i)
+    else:
+        odd.append(i)
+print("even number is:",even)
+print("odd number is:",odd)
+        
